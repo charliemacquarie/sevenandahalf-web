f9d26e31f9bdae47967c3d3a5d339de2338bb9c9
charliemacquarie <7698633+charliemacquarie@users.noreply.github.com>
2021-02-23 21:12:49 -0800

    add init_db

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import os
-from flask import Flask, render_template, request
+import sqlite3
+
+import click
+from flask import (
+    Flask, render_template, request, current_app
+)
+from flask.cli import with_appcontext
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
@@ -11,6 +17,22 @@ try:
     os.makedirs(app.instance_path)
 except OSError:
     pass
+
+def init_db():
+    conn = sqlite3.connect(current_app.config['DATABASE'])
+    c = conn.cursor()
+
+    with current_app.open_resource('schema.sql') as f:
+        c.executescript(f.read().decode('utf-8'))
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear existing data and create new map table"""
+    init_db()
+    click.echo('Initialized the database.')
+
+app.cli.add_command(init_db_command)
 
 @app.route('/', methods=('GET', 'POST'))
 def main():
@@ -32,6 +54,7 @@ def show_configs():
     configs.append('Instance path: {}'.format(app.instance_path))
     configs.append('app.config: {}'.format(app.config))
     configs.append('__name__: {}'.format(__name__))
+    configs.append('current_app.config: {}'.format(current_app.config))
 
     return render_template('config.html', configs=configs)
 
