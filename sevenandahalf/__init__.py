2bb067dae46675546ede4a27396c2a7bed45ad99
charliemacquarie <7698633+charliemacquarie@users.noreply.github.com>
2021-03-30 13:03:54 -0700

    create db module

--- sevenandahalf/__init__.py
+++ sevenandahalf/__init__.py
@@ -6,7 +6,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'maps.sqlite'),
     )
 
     if test_config is None:
@@ -22,5 +22,8 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello you freakies'
+
+    from . import db
+    db.init_app(app)
 
     return app
--- sevenandahalf/app.py
+++ sevenandahalf/app.py
@@ -20,42 +20,6 @@ try:
 except OSError:
     pass
 
-def init_db():
-    conn = sqlite3.connect(
-        current_app.config['DATABASE'],
-        detect_types=sqlite3.PARSE_DECLTYPES
-    )
-    c = conn.cursor()
-
-    # read in file to clear any existing data and make empty table
-    with current_app.open_resource('schema.sql') as f:
-        c.executescript(f.read().decode('utf-8'))
-
-    # read in csv file with map data
-    maps = []
-    with current_app.open_resource('initialize.csv', 'r') as csvf:
-        reader = csv.reader(csvf)
-        for row in reader:
-            maps.append(row)
-
-    # insert map data to db and close the connection
-    c.executemany('''INSERT INTO map VALUES (?,?,?,?,?,?,?,?,?,?,?,?,
-    ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,
-    ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', maps)
-    conn.commit()
-    conn.close()
-
-# create the init-db command
-@click.command('init-db')
-@with_appcontext
-def init_db_command():
-    """Clear existing data and create new map table"""
-    init_db()
-    click.echo('Initialized the map database.')
-
-# register the db init command with the app
-app.cli.add_command(init_db_command)
-
 @app.route('/', methods=('GET', 'POST'))
 def main():
     if request.method == 'POST':
